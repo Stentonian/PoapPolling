f6a9074f7d6d6116e9ab20465653c6a4287b85c6
Jason <jason@smythemail.za.net>
2019-08-03 11:48:13 +0200

    Add deployment config truffle

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HDWalletProvider = require('truffle-hdwallet-provider');
+// This is throwaway keys for hackathon ONLY!! Please never use again, EVER!
+const mnemonic = 'dumb gown nose volume total father license lady empower snow body tag'; 
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +10,13 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    matic: {
+      network_id: '*',
+      provider: new HDWalletProvider(mnemonic, 'https://testnet2.matic.network', 0),
+      gas: 4700000,
+      gasPrice: 0,
+      skipDryRun: true,
     }
   }
 };
