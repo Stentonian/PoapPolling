813ef74c4c48d71bddad2227842a976e700bd3ef
Sean Markham <mrksea002@uct.ac.za>
2019-08-03 19:00:51 +0530

    Edits to migrations and truffle-config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,16 @@
+var Poll = artifacts.require("./Poll.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var Verifier = artifacts.require("./Verifier.sol");
+
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Verifier);
+  deployer.deploy(Verifier).then(function() {
+    return deployer.deploy(Poll, Verifier.address);
+  });
+
+  deployer.deploy(Ownable);
+
   deployer.deploy(SimpleStorage);
 };
