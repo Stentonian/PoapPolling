6485aa558d5925c31c42895f60e7f4f25dbbe594
Sean Markham <mrksea002@uct.ac.za>
2019-08-04 07:30:00 +0530

    Updates

---
+++ precache-manifest.cffa87d97497ff5e62da7e8f3612070d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a41db5c286f61914f77602cbce9131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b6c36d5b9ce4945675",
+    "url": "/static/css/main.e759e63b.chunk.css"
+  },
+  {
+    "revision": "677b8212dd1c8ed374ad",
+    "url": "/static/js/2.01b65b14.chunk.js"
+  },
+  {
+    "revision": "64b6c36d5b9ce4945675",
+    "url": "/static/js/main.3e226849.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1bc024be719c84b55d6b2fd1b8d4743d",
+    "url": "/static/media/poll.1bc024be.svg"
+  }
+]);
